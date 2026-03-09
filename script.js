c25222b1ddd3bfb4884ff7a2fd6f74a2b6a42bad
sandhya31-ops <sandylamichhane06@gmail.com>
2026-03-09 19:45:16 +0545

    my frontend workshop

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 // array
-const arr = new Array("helloo","fahh")
-console.log(arr)
+// const arr = new Array("helloo","fahh")
+// console.log(arr)
+// const num =  new Array(1,2,3,4,5)
+// console.log(num.length);
+const bar = ["low","high"]
+console.log(bar[1])
